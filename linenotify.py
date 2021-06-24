4bd60588ff68ea20afc0d424c85f6c1b9c2efcb1
mkmysk123456789 <mkmysk123456789@gmail.com>
2021-06-25 04:50:24 +0900

    line notify group changed

--- linenotify.py
+++ linenotify.py
@@ -1,12 +1,12 @@
 import requests
 
 
-def send_line_notify():
+def send_line_notify(message='Just Finished Learning.'):
     """
     LINEに通知する
     """
-    line_notify_token = 'AqDWTPO25wqUrP8qSosG9JBpuwKhTKvmf8AUyg99Bh4'
+    line_notify_token = 'yDrpFWiOnY1rgsNqBmAcTIKMPbUsMqicfVronvuGmtF'
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
-    data = {'message': f'message: finished'}
+    data = {'message': message}
     requests.post(line_notify_api, headers=headers, data=data)
