95e9b676b21e8423f18d70e8e5700522e3681257
mkmysk123456789 <mkmysk123456789@gmail.com>
2021-06-27 03:31:33 +0900

    load changed

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -282,8 +282,6 @@ class Exp_Informer(Exp_Basic):
         preds = []
         trues = []
 
-        
-
         # i = 0 しか実行しない bach = 1??
         for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
             # どこの予測??
@@ -303,18 +301,18 @@ class Exp_Informer(Exp_Basic):
 
         trues = np.array(trues)
         trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
-        
+
         mae, mse, rmse, mape, mspe = metric(preds, trues)
         print('mse:{}, mae:{}'.format(mse, mae))
 
         # result save
         folder_path = './results/' + setting + '/'
-        
+
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
-            
+
         np.save(folder_path+'metrics.npy',
-        np.array([mae, mse, rmse, mape, mspe]))
+                np.array([mae, mse, rmse, mape, mspe]))
 
         np.save(folder_path+'pred_preds.npy', preds)
         np.save(folder_path+'pred_trues.npy', trues)
