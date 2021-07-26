6bd3addcdafc33a85ab368c6941c51d14da22b78
mkmysk123456789 <mkmysk123456789@gmail.com>
2021-07-26 22:05:46 +0900

    main changed

--- main_informer.py
+++ main_informer.py
@@ -132,7 +132,7 @@ data_parser = {
     'ETTm1': {'data': 'ETTm1.csv', 'T': 'OT', 'M': [7, 7, 7], 'S': [1, 1, 1], 'MS': [7, 7, 1]},
     'ETTm2': {'data': 'ETTm2.csv', 'T': 'OT', 'M': [7, 7, 7], 'S': [1, 1, 1], 'MS': [7, 7, 1]},
     'WTH': {'data': 'WTH.csv', 'T': 'WetBulbCelsius', 'M': [12, 12, 12], 'S': [1, 1, 1], 'MS': [12, 12, 1]},
-    'LCD': {'data': 'LCD.csv', 'T': 'HourlyWetBulbTemperature', 'M': [12, 12, 12], 'S': [1, 1, 1], 'MS': [12, 12, 1]},
+    'LCD': {'data': 'LCD.csv', 'T': 'HourlyWetBulbTemperature', 'M': [8, 8, 8], 'S': [1, 1, 1], 'MS': [8, 8, 1]},
     'ECL': {'data': 'ECL.csv', 'T': 'MT_320', 'M': [321, 321, 321], 'S': [1, 1, 1], 'MS': [321, 321, 1]},
     'Solar': {'data': 'solar_AL.csv', 'T': 'POWER_136', 'M': [137, 137, 137], 'S': [1, 1, 1], 'MS': [137, 137, 1]},
 }
